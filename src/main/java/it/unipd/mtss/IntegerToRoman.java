f0bbe71395b828df0885c4752994714fe9b0c004
Estintore <giopossenti3@gmail.com>
2026-05-05 16:24:53 +0200

    Convertiti i numeri fino a 500 e test (closes #12)

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -9,7 +9,9 @@ public class IntegerToRoman {
 
     public static String convert(int numero) {
         int[] valori = {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] simboli = {"D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] simboli = {
+            "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
         
         String roman = "";
         
